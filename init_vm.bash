9d745ff8e51ab51c6acfead83b68faefd3549e33
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-06-08 15:55:05 +0200

    added php5-mcrypt to dependencies

--- init_vm.bash
+++ init_vm.bash
@@ -6,7 +6,7 @@ apt-get update
 echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
 echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential apache2 php5-gd php5-sqlite php5 php5-cli php5-mysql mysql-server php-db php-pear
+DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential apache2 php5-gd php5-sqlite php5 php5-cli php5-mysql mysql-server php-db php-pear php5-mcrypt
 
 pecl install uploadprogress
 echo "extension=uploadprogress.so" > /etc/php5/apache2/conf.d/uploadprogress.ini
@@ -19,6 +19,8 @@ chown root:root /etc/php5/apache2/php.ini
 cp /root/setup/mpm_prefork.conf /etc/apache2/mods-available/mpm_prefork.conf
 chown root:root /etc/apache2/mods-available/mpm_prefork.conf
 a2enmod rewrite
+php5enmod mcrypt
+
 mkdir /etc/my_init.d
 cp /root/setup/init_container.sh /etc/my_init.d/init_container.sh
 chown root:root /etc/my_init.d/init_container.sh
